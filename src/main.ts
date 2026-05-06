c8ef8f8db36a31e3aa50fc67dc7a4e508a2d31fd
Majo Bacca <majobacca0234@gmail.com>
2026-05-05 22:39:41 -0500

    fix: enable CORS with origin true for development

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,18 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  //(HABILITAR CORS)
+  app.enableCors({
+    origin: [
+      'http://localhost:3000',
+      'http://localhost:3001',
+      'http://localhost:3002',
+      'https://ludix-frontend.vercel.app',
+    ],
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    credentials: true,
+  });
+
   app.useGlobalPipes(new ValidationPipe());
 
   const config = new DocumentBuilder()
